d25c5a4a7a23952b70ac5100448eef58938206d7
Andrew Davisson <akdavisson4@gmail.com>
2020-08-20 11:41:20 -0600

    made snake component

---
+++ src/components/Snake.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="snake"></div>
+</template>
+
+<script>
+export default {
+  name: "Snake",
+  data: () => ({
+    length: 1
+  })
+};
+</script>
+
+<style scoped>
+.snake {
+  height: 10px;
+  width: 10px;
+  background-color: black;
+}
+</style>
