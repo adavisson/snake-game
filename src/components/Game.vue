16bdd2b06b4216c1aca371cec0cef2c783da9592
Andrew Davisson <akdavisson4@gmail.com>
2020-08-20 09:13:46 -0600

    added game board

---
+++ src/components/Game.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="snake">
+    <h3>Snake</h3>
+    <div class="game-area"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Game",
+  data: () => ({}),
+}
+</script>
+
+<style scoped>
+  .snake {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .game-area {
+    background-color: white;
+    width: 500px;
+    height: 500px;
+  }
+</style>
