322ebb57c4afb30e636cd33db40e49a2170dae71
Andrew Davisson <akdavisson4@gmail.com>
2020-08-20 11:41:39 -0600

    try to fill board with grid

---
+++ src/components/Board.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="game-area">
+    <div v-for="(square, index) in grid" :key="index" class="space"></div>
+  </div>
+</template>
+
+<script>
+// import Snake from "./Snake"
+
+export default {
+  name: "Board",
+  components: {
+    // Snake,
+  },
+  data: () => ({
+    grid: []
+  }),
+  beforeMount() {
+    this.grid = this.fillGrid()
+  },
+  methods: {
+    fillGrid() {
+      let spaces = []
+      for (let i = 0; i < 50; i++) {
+        spaces.push([]);
+        for (let j = 0; i < 50; j++) {
+          spaces[i].push(0);
+        }
+      }
+      return spaces
+    }
+  }
+};
+</script>
+
+<style scoped>
+.game-area {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  background-color: white;
+  width: 500px;
+  height: 500px;
+}
+.space {
+  width: 10px;
+  height: 10px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: black;
+}
+</style>
